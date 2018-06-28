f267d7026ebc9eeabd2a93be88f7d96f64df8acd
HoratiuWK <horatiu2004@gmail.com>
2018-06-28 16:24:18 +0300

    WebApplication first draft

---
+++ WebApplication1/WebApplication1/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WebApplication1.Models
+{
+    public enum UserType
+    {
+        [Description("Standard")]
+        Standard = 1,
+        [Description("PowerUser")]
+        PowerUser,
+        [Description("Admin")]
+        Admin
+    }
+
+    public class User
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public UserType Type { get; set; }
+    }
+}
