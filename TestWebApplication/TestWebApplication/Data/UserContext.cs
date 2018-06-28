471efc4aaedfd35bb17ce71e922708b53f8d44e8
HoratiuWK <horatiu2004@gmail.com>
2018-06-28 17:59:34 +0300

    Basic web apps

---
+++ TestWebApplication/TestWebApplication/Data/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestWebApplication.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext (DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestWebApplication.Models.User> User { get; set; }
+    }
+}
---
+++ TestWebApplication/TestWebApplication/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebApplication.Models
+{
+    public enum UserType
+    {
+        [Display(Name = "Standard")]
+        Standard,
+        [Display(Name = "PowerUser")]
+        PowerUser,
+        [Display(Name = "Admin")]
+        Admin
+    }
+
+    public class User
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public UserType Type { get; set; }
+    }
+}
