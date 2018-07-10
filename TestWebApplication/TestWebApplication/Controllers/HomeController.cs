0ea7f4aac1ccf4d573610449523e7c00f0449971
HoratiuWK <horatiu2004@gmail.com>
2018-07-10 16:49:01 +0300

    New account page

--- TestWebApplication/TestWebApplication/Controllers/HomeController.cs
+++ TestWebApplication/TestWebApplication/Controllers/HomeController.cs
@@ -29,6 +29,13 @@ namespace TestWebApplication.Controllers
             return View();
         }
 
+        public IActionResult Account()
+        {
+            ViewData["Message"] = "Your account page.";
+
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
